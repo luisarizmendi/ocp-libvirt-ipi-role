b2d6d7d0e7f5a5d28af036f6ba934a3655b51f9a
luisarizmendi <luis.ariz@gmail.com>
2021-08-04 14:30:39 +0200

    Changes in NFS autoprovisioner

--- files/post-install-scripts/nfs-autoprovisioner/run.sh
+++ files/post-install-scripts/nfs-autoprovisioner/run.sh
@@ -4,6 +4,9 @@ echo "****************************"
 echo "Configuring Dynamic NFS"
 echo "****************************"
 
+
 oc process -f template.yaml --param-file=env | oc create -f -
 
-oc adm policy add-scc-to-user hostmount-anyuid -n nfs-autoprovisioner -z nfs-client-provisioner
+
+oc adm policy add-scc-to-user hostmount-anyuid system:serviceaccount:nfs-autoprovisioner:nfs-client-provisioner
+
